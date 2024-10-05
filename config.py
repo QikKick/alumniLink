ead0a41d533365d3d665e39da49b86c6a9a576ff
Nerte <nerte@Nertes-MacBook-Pro.local>
2024-10-05 12:10:32 +0300

    db

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    username = 'postgres'
+    password = ''
+    database = 'postgres'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'postgresql://{username}:{password}@localhost:5432/{database}')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
