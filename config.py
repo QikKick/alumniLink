104fb33efc13bdf19dabb24361aef5b70f00f0ff
Nerte <nerte@Nertes-MacBook-Pro.local>
2024-10-05 14:46:14 +0300

    db

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ class Config:
     database = 'postgres'
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'postgresql://{username}:{password}@localhost:5432/{database}')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # flask db init
+    # flask db migrate -m "Initial migration."
+    # flask db upgrade
